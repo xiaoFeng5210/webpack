5b936f063edc1334c83e178dbb8fcb022fbcae53
Eadweard <804875728@qq.com>
2022-08-25 22:34:24 +0800

    less loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,14 @@ module.exports = {
                         ]
                     }
                 }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
             }
         ]
     }
