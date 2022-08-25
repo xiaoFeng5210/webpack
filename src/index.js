c41f6bb187421cedf6509492276fd2609b2f84fe
Eadweard <804875728@qq.com>
2022-08-25 22:26:21 +0800

    fix bug

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const xiaofeng = "张庆风"
+console.log(xiaofeng)
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 module.exports = {
     mode: 'production',
+    entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
