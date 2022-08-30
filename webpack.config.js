8400320d9c00efd715760e5492e181d605ae46e1
Eadweard <804875728@qq.com>
2022-08-30 22:53:34 +0800

    支持less文件

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,23 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.less$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                compileType: 'icss',
+                            },
+                        },
+                    },
+                    {
+                        loader: 'less-loader',
+                    }
+                ]
+            },
+            {
                 test: /\.[jt]sx?$/,
                 exclude: /node_modules/,
                 use: {
